e8cbfe1eb877af66d2054c2d620f03cd7f3f90da
Lucas Maystre <lucas@maystre.ch>
2011-12-30 15:18:30 +0100

    tests of the big integer class

--- EasyModulo/MainPage.xaml.cs
+++ EasyModulo/MainPage.xaml.cs
@@ -20,5 +20,12 @@ namespace EasyModulo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            BigInteger n1 = new BigInteger("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999",10);
+            BigInteger n2 = new BigInteger("9999222222222222299999998888888",10);
+            textBlock1.Text = (n1.modPow(1,n2)).ToString();
+        }
     }
 }
